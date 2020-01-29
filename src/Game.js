010272c03da76ea1d26a9ef910ad1d8fee2a749d
kevin johnson <kevinj902302@gmail.com>
2020-01-29 15:58:47 -0700

    Created game start function

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
 
 class Game {
-  constructor() {}
+  constructor() {
+    
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +17,13 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start() {
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    this.printMessage(deck, round);
+    this.printQuestion(round);
   }
 }
 
