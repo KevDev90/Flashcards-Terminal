58e04cdb6734e87c765e234cc2410299074d0007
kevin johnson <kevinj902302@gmail.com>
2020-01-28 22:14:47 -0700

    Added round class methods

--- src/round.js
+++ src/round.js
@@ -4,6 +4,26 @@ class Round {
     this.turns = 0;
     this.incorrectAnswers = [];
   }
+
+  returnCurrentCard() {
+    return this.deck.cards[this.turns];
+  }
+
+  takeTurn(guess) {
+    const turn = new Turn(guess, this.returnCurrentCard());
+    !turn.evaluateGuess() ? this.incorrectAnswers.push(this.returnCurrentCard().id) : null;
+    this.turns++;
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    const incorrect = this.incorrectAnswers.length;
+    return Math.round(incorrect / this.turns * 100);
+  }
+
+  endRound() {
+   return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
 }
 
 module.exports = Round;
